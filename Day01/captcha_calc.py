128ecb40555fa42a4eaa912b553d79759ef9ca78
spelinski <rohk@hotmail.com>
2017-12-01 13:46:41 -0600

    remove get list function I had added when it was originally duplicated

--- Day01/captcha_calc.py
+++ Day01/captcha_calc.py
@@ -1,10 +1,6 @@
-def get_list_of_numbers(captcha_string):
-    return [int(x) for x in captcha_string]
-
-
 def add_matching_items(captcha_string, distance_of_compare):
     total = 0
-    list_of_ints = get_list_of_numbers(captcha_string)
+    list_of_ints = [int(x) for x in captcha_string]
     for index, value in enumerate(list_of_ints, start=0):
         compare_index = (index + distance_of_compare) % len(list_of_ints)
         if value == list_of_ints[compare_index]:
