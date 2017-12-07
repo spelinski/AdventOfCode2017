ec2042ae5a55b29c52faaaae3ac6a00146bbbf12
spelinski <rohk@hotmail.com>
2017-12-07 16:24:12 -0600

    getting started'

--- Day07/src/lib.rs
+++ Day07/src/lib.rs
@@ -1,6 +1,25 @@
+use std::env;
+use std::fs::File;
+use std::io::prelude::*;
+
+pub fn find_bottom(filename: String) -> String {
+    println!("In file {}", filename);
+
+    let mut f = File::open(filename).expect("file not found");
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        .expect("something went wrong reading the file");
+
+    println!("With text:\n{}", contents);
+    "abcd".to_string()
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
-    fn it_works() {
+    fn testing_find_bottom() {
+        assert_eq!("tknk".to_string(),find_bottom("test_data.txt".to_string()));
     }
 }
